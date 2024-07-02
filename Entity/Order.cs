727cb3cd54dc78068201f240a562e84ae96e6d07
Do Hoang Cong Minh <dhcongminh@gmail.com>
2024-07-02 14:05:07 +0700

    create order and orderdetail model to test page

---
+++ Entity/Order.cs
@@ -0,0 +1,26 @@
+﻿namespace EvoltingStore.Entity {
+    public class Order {
+        /*
+         Order:
+            orderId
+            userId
+            totalPrice
+            orderDate
+            payment
+            status
+        */
+        public Order() {
+            OrderDetails = new HashSet<OrderDetail>();
+        }
+
+        public int OrderId {  get; set; }
+        public int UserId { get; set; }
+        public double TotalPrice { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string? Payment {  get; set; }
+        public string? Status {  get; set; }
+
+        public virtual User User { get; set; } = null!;
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+}
---
+++ Entity/OrderDetail.cs
@@ -0,0 +1,10 @@
+﻿namespace EvoltingStore.Entity {
+    public class OrderDetail {
+        public int OrderDetailId { get; set; }
+        public int OrderId { get; set; }
+        public int GameId { get; set; }
+
+        public virtual Order Order { get; set; } = null!;
+        public virtual Game Game { get; set; } = null!;
+    }
+}
