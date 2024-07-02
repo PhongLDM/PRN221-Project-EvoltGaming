50bbc133818f38d910d5a90369cc8eb2be2989b5
Do Hoang Cong Minh <dhcongminh@gmail.com>
2024-07-03 01:27:57 +0700

    sửa order page theo model gen ra từ database mới

--- Pages/Management/Orders/Index.cshtml.cs
+++ Pages/Management/Orders/Index.cshtml.cs
@@ -12,27 +12,7 @@ namespace EvoltingStore.Pages.Management.Orders
             String userJSON = (String)HttpContext.Session.GetString("user");
             User user = (User)Newtonsoft.Json.JsonConvert.DeserializeObject<User>(userJSON);
             if (user.RoleId == 1) {
-
-                ICollection<Order> orders = new List<Order>();
-                Order order = new Order();
-                order.OrderId = 1;
-                order.Status = "Shipping";
-                order.TotalPrice = 12f;
-                order.OrderDate = DateTime.Now;
-                order.Payment = "COD";
-                order.UserId = 1;
-                order.OrderDetails = new List<OrderDetail>();
-                OrderDetail orderDetail = new OrderDetail();
-                orderDetail.OrderId = 1;
-                orderDetail.Order = order;
-                orderDetail.GameId = 1;
-                order.OrderDetails.Add(orderDetail);
-                orders.Add(order);
-
-
-
-
-                ViewData["orders"] = orders;
+                ViewData["orders"] = context.Orders.ToList();
 
                 return Page();
             }
