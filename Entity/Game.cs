e9d97721b4e316722d2708bdb7cea061f0beb145
Dzung Phung <dungdzaj255@gmail.com>
2024-07-02 18:22:29 +0700

    Generate entities

--- Entity/Game.cs
+++ Entity/Game.cs
@@ -7,8 +7,10 @@ namespace EvoltingStore.Entity
     {
         public Game()
         {
+            CartItems = new HashSet<CartItem>();
             Comments = new HashSet<Comment>();
             GameRequirements = new HashSet<GameRequirement>();
+            OrderDetails = new HashSet<OrderDetail>();
             Genres = new HashSet<Genre>();
             Users = new HashSet<User>();
         }
@@ -20,11 +22,13 @@ namespace EvoltingStore.Entity
         public DateTime ReleaseDate { get; set; }
         public DateTime UpdateDate { get; set; }
         public string Platform { get; set; } = null!;
-        public string Image { get; set; }
+        public string? Image { get; set; }
         public double Price { get; set; }
 
+        public virtual ICollection<CartItem> CartItems { get; set; }
         public virtual ICollection<Comment> Comments { get; set; }
         public virtual ICollection<GameRequirement> GameRequirements { get; set; }
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
 
         public virtual ICollection<Genre> Genres { get; set; }
         public virtual ICollection<User> Users { get; set; }
