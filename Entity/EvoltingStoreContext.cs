2b6db01754387cc1185255a9fe5960b282832aa9
PhiNX <xuanphi2003@gmail.com>
2024-07-21 21:47:53 +0700

    fix context and blog

--- Entity/EvoltingStoreContext.cs
+++ Entity/EvoltingStoreContext.cs
@@ -33,8 +33,8 @@ namespace EvoltingStore.Entity
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server=DZUNG-PC\\DUNG;database=EvoltingStore;Integrated Security=true;TrustServerCertificate=true");
+                var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+                optionsBuilder.UseSqlServer(config.GetConnectionString("EvoltingStore"));
             }
         }
 
